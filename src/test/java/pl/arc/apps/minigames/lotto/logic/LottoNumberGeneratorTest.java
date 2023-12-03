c6d8340e0bbf5f50e0c3594f1b305f49e5302d0b
klakd <arkadiusz.klak@gmail.com>
2023-12-03 15:56:08 +0100

    Refactored unit test for LottoNumberGenerator

--- src/test/java/pl/arc/apps/minigames/lotto/logic/LottoNumberGeneratorTest.java
+++ src/test/java/pl/arc/apps/minigames/lotto/logic/LottoNumberGeneratorTest.java
@@ -11,19 +11,36 @@ import static org.junit.jupiter.api.Assertions.*;
 class LottoNumberGeneratorTest {
 
     @Test
-    void shouldReturnSetWithSixUniqueNumbers() {
+    void shouldGenerateSetWithSixNumbers() {
         // Given
         int min = LottoConstants.MIN_LOTTO_NUMBER;
         int max = LottoConstants.MAX_LOTTO_NUMBER;
-
         // When
         Set<Integer> lottoNumbers = LottoNumberGenerator.generateLottoNumbers(min, max, LottoConstants.NUMBERS_TO_PICK);
-
         // Then
-        assertEquals(LottoConstants.NUMBERS_TO_PICK, lottoNumbers.size(),
-                "Set should contain 6 numbers");
+        assertEquals(LottoConstants.NUMBERS_TO_PICK, lottoNumbers.size(), "Set should contain 6 numbers");
+    }
+
+    @Test
+    void shouldGenerateNumbersWithinSpecifiedRange() {
+        // Given
+        int min = LottoConstants.MIN_LOTTO_NUMBER;
+        int max = LottoConstants.MAX_LOTTO_NUMBER;
+        // When
+        Set<Integer> lottoNumbers = LottoNumberGenerator.generateLottoNumbers(min, max, LottoConstants.NUMBERS_TO_PICK);
+        // Then
         assertTrue(lottoNumbers.stream().allMatch(num -> num >= min && num <= max),
                 "All numbers should be within the specified range");
+    }
+
+    @Test
+    void shouldGenerateSetWithUniqueNumbers() {
+        // Given
+        int min = LottoConstants.MIN_LOTTO_NUMBER;
+        int max = LottoConstants.MAX_LOTTO_NUMBER;
+        // When
+        Set<Integer> lottoNumbers = LottoNumberGenerator.generateLottoNumbers(min, max, LottoConstants.NUMBERS_TO_PICK);
+        // Then
         assertEquals(LottoConstants.NUMBERS_TO_PICK, new HashSet<>(lottoNumbers).size(),
                 "All numbers should be unique");
     }
