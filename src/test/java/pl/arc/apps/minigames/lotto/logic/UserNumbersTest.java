a366f1d3b611767f4b618070a8567206956ab504
klakd <arkadiusz.klak@gmail.com>
2023-12-09 17:46:00 +0100

    Added UserNumbersTest

---
+++ src/test/java/pl/arc/apps/minigames/lotto/logic/UserNumbersTest.java
@@ -0,0 +1,39 @@
+package pl.arc.apps.minigames.lotto.logic;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import pl.arc.apps.minigames.lotto.common.LottoConstants;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class UserNumbersTest {
+
+    @Mock
+    private Scanner scanner;
+
+    @Test
+    void shouldEnterUserNumbers() {
+        MockitoAnnotations.openMocks(this);
+
+        when(scanner.nextInt()).thenReturn(1, 2, 3, 4, 5, 6);
+
+        UserNumbers userNumbers = new UserNumbers();
+        Set<Integer> expectedNumbers = new HashSet<>();
+        for (int i = 1; i <= 6; i++) {
+            expectedNumbers.add(i);
+        }
+
+        userNumbers.enterUserNumbers(scanner, LottoConstants.MIN_LOTTO_NUMBER, LottoConstants.MAX_LOTTO_NUMBER,
+                LottoConstants.NUMBERS_TO_PICK);
+
+        assertEquals(expectedNumbers, userNumbers.getUserNumbers(), "User numbers should match expected numbers");
+
+        verify(scanner, times(6)).nextInt();
+    }
+}
